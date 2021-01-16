90219ebe1a56814f2f03d70397785e30c549d09b
Wenshan <wenshan.wu@saynova.com>
2021-01-16 18:50:33 +0100

    class constellation network

---
+++ code/constellationnet.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import torch
+
+class ConstellationNet(torch.nn.Module):
+    
+    def __init__(self, x_size):
+        super(ConstellationNet, self).__init__()
+        
+        
+    def forward(self, x):
+        pass
+    
+    def conv(self):
+        pass
+    
+    def constell(self):
+        pass
